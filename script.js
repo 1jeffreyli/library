dc4150a9052adeda9ceb52851612d811bac7bac3
Jeffrey Li <lijeffre@grinnell.edu>
2022-08-05 15:00:32 -0700

    Format code using prettier

--- script.js
+++ script.js
@@ -2,23 +2,30 @@ let myLibrary = [];
 
 // constructor
 function Book(title, author, pages, read) {
-	this.title = title
-	this.author = author
-	this.pages = pages
-	this.read = read	
-	this.info = function () {
-		return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read + ".";	
-		}
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return (
+      this.title +
+      " by " +
+      this.author +
+      ", " +
+      this.pages +
+      " pages, " +
+      this.read +
+      "."
+    );
+  };
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
 
-
 function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
-    myLibrary.push(book);
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
 }
-
 
 // a function that loops through the myLibrary array and displays them in the table
 // two loops, first one to create a row element for each book, the second nested loop to create and append
@@ -26,15 +33,15 @@ function addBookToLibrary(title, author, pages, read) {
 
 let bookProperties = ["title", "author", "pages", "read"];
 
-function displayBook (myLibrary) {
-    const table = document.getElementById("table");
-    for (let i = 0; i < myLibrary.length; i++) {
-        const row = table.insertRow(-1);
-        for (let j = 0; j <= myLibrary.length + 1; j++) {
-            const cell = row.insertCell(-1);
-            cell.innerHTML = myLibrary[i][bookProperties[j]];
-        }
+function displayBook(myLibrary) {
+  const table = document.getElementById("table");
+  for (let i = 0; i < myLibrary.length; i++) {
+    const row = table.insertRow(-1);
+    for (let j = 0; j <= myLibrary.length + 1; j++) {
+      const cell = row.insertCell(-1);
+      cell.innerHTML = myLibrary[i][bookProperties[j]];
     }
+  }
 }
 
 // Create a modal for the NEW BOOK form and set the position to be absolute, with the view toggled from hidden to visible on click event
@@ -43,12 +50,13 @@ function displayBook (myLibrary) {
 // add an event listener to the New Book button that pops out the modal by removing display none
 const newBookBtn = document.getElementById("new-book-btn");
 function displayToggle() {
-    const modal = document.getElementById("modal");
-    modal.classList.remove("hidden");
+  const modal = document.getElementById("modal");
+  modal.classList.remove("hidden");
 }
 newBookBtn.addEventListener("click", displayToggle);
 
 // add an event listener to the Add Book button to submit the form's inputs and call the addBookToLibrary function and displayBook function
 // also closing the modal by adding display none back to it
+
 // separately add event listeners to the x button and blackish background that "close" the modal by
 // adding display none back to the modal
