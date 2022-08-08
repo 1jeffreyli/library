16d35123571907f3759aee689904c71bef54e356
Jeffrey Li <lijeffre@grinnell.edu>
2022-08-08 16:11:32 -0700

    Refactor display functionality and styling

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let myLibrary = [
-    // {
-    //     title: "The Hobbit",
-    //     author: "J.R.R. Tolkien",
-    //     pages: 295,
-    //     read: "no"
-    // },
-    // {
-    //     title: "Harry Potter",
-    //     author: "JK Rowling",
-    //     pages: 123,
-    //     read: "yes"
-    // }
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        pages: 295,
+        read: "no"
+    },
+    {
+        title: "Harry Potter",
+        author: "JK Rowling",
+        pages: 123,
+        read: "yes"
+    }
 ];
 
 // DOM objects
@@ -18,40 +18,45 @@ const newBookButton = document.querySelector("#new-book-btn");
 const bookCards = document.querySelector(".book-cards");
 
 // constructor
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  };
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+};
   
   // a function that creates a div with the title as a header and appends it to book-cards container
   function displayBook(myLibrary) {
-      for (let i=0; i < myLibrary.length; i++) {
+    bookCards.innerHTML = "";
+      for (let i= 0; i < myLibrary.length; i++) {
           const div = document.createElement("div");
-          const h1 = document.createElement("h2");
+          const h2 = document.createElement("h2");
           const p = document.createElement("p");
-          h1.textContent = myLibrary[i]["title"];
-          p.textContent = myLibrary[i]["author"] + " " + myLibrary[i]["pages"] + " " + myLibrary[i]["read"];
-          div.appendChild(h1);
+          h2.textContent = myLibrary[i]["title"];
+          p.textContent = "By " + myLibrary[i]["author"] + ", " + myLibrary[i]["pages"] + " pages, " + myLibrary[i]["read"];
+          div.appendChild(h2);
           div.appendChild(p);
           bookCards.appendChild(div);
         }
     }
-    // displayBook(myLibrary);
+    displayBook(myLibrary);
     
-    function addBookToLibrary() {
-        const title = document.querySelector("#book-title").value;
-        const author = document.querySelector("#author").value;
-        const pages = document.querySelector("#pages").value;
-        const read = document.querySelector("#read").value;
-        const form = document.getElementById("form").submit();
+    function addBookToLibrary(title, author, pages, read) {
+        // const title = document.querySelector("#book-title").value;
+        // const author = document.querySelector("#author").value;
+        // const pages = document.querySelector("#pages").value;
+        // const read = document.querySelector("#read").value;
+        // const form = document.getElementById("form").submit();
         // const modalForm = document.querySelector(".modal-form");
-        let newBook = new Book(title, author, pages, read);
+        newBook = new Book(title, author, pages, read);
         myLibrary.push(newBook);
+        // form.addEventListener("click", displayBook(myLibrary));
         // addBookToLibrary();
-        displayBook(myLibrary);
+        // displayBook(myLibrary);
     }
+    // addBookToLibrary();
 const addBookButton = document.querySelector(".add-book-btn");
 addBookButton.addEventListener("click", addBookToLibrary);
 
@@ -65,8 +70,6 @@ newBookBtn.addEventListener("click", displayToggle);
 
 // add an event listener to the Add Book button to submit the form's inputs and call the addBookToLibrary function and displayBook function
 // also closing the modal by adding display none back to it
-// const bookTitle = document.getElementById("book-title").value;
-// const bookAuthor = document.getElementById("author").value;
 
 
 // event listeners to hide the modal when clicking the cancel button, modal background, or pressing the escape key
