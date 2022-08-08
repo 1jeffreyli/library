0e863b82626a416a4dbfad1ec97b458d6eed38a5
Jeffrey Li <lijeffre@grinnell.edu>
2022-08-08 14:50:21 -0700

    Add display functionality

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
 let myLibrary = [
-    {
-        title: "The Hobbit",
-        author: "J.R.R. Tolkien",
-        pages: 295,
-        read: "no"
-    },
-    {
-        title: "Harry Potter",
-        author: "JK Rowling",
-        pages: 123,
-        read: "yes"
-    }
+    // {
+    //     title: "The Hobbit",
+    //     author: "J.R.R. Tolkien",
+    //     pages: 295,
+    //     read: "no"
+    // },
+    // {
+    //     title: "Harry Potter",
+    //     author: "JK Rowling",
+    //     pages: 123,
+    //     read: "yes"
+    // }
 ];
 
 // DOM objects
 const newBookButton = document.querySelector("#new-book-btn");
-const table = document.querySelector(".book-table");
-const tbody = document.querySelector("tbody");
-
-const modalForm = document.querySelector(".modal-form");
-const title = document.querySelector("#book-title");
-const author = document.querySelector("#author");
-const pages = document.querySelector("#pages");
-const read = document.querySelector("#read");
 const bookCards = document.querySelector(".book-cards");
 
 // constructor
@@ -32,41 +24,36 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
   };
-
-
-const addBookToLibrary = () => {
-    let title = titleInput.value;
-    let author = authorInput.value;
-    let pages = pagesInput.value;
-    let read = readValue();
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-}
+  
+  // a function that creates a div with the title as a header and appends it to book-cards container
+  function displayBook(myLibrary) {
+      for (let i=0; i < myLibrary.length; i++) {
+          const div = document.createElement("div");
+          const h1 = document.createElement("h2");
+          const p = document.createElement("p");
+          h1.textContent = myLibrary[i]["title"];
+          p.textContent = myLibrary[i]["author"] + " " + myLibrary[i]["pages"] + " " + myLibrary[i]["read"];
+          div.appendChild(h1);
+          div.appendChild(p);
+          bookCards.appendChild(div);
+        }
+    }
+    // displayBook(myLibrary);
+    
+    function addBookToLibrary() {
+        const title = document.querySelector("#book-title").value;
+        const author = document.querySelector("#author").value;
+        const pages = document.querySelector("#pages").value;
+        const read = document.querySelector("#read").value;
+        const form = document.getElementById("form").submit();
+        // const modalForm = document.querySelector(".modal-form");
+        let newBook = new Book(title, author, pages, read);
+        myLibrary.push(newBook);
+        // addBookToLibrary();
+        displayBook(myLibrary);
+    }
 const addBookButton = document.querySelector(".add-book-btn");
 addBookButton.addEventListener("click", addBookToLibrary);
-
-function readValue() {
-    if(form.querySelector('input[name="read"]'.value == "yes")) {
-        return true;
-    } else {
-    return false;
-    }
-}
-
-// a function that creates a div with the title as a header and appends it to book-cards container
-function displayBook(myLibrary) {
-    for (let i=0; i < myLibrary.length; i++) {
-    const div = document.createElement("div");
-    const h1 = document.createElement("h2");
-    const p = document.createElement("p");
-    h1.textContent = myLibrary[i]["title"];
-    p.textContent = myLibrary[i]["author"] + " " + myLibrary[i]["pages"] + " " + myLibrary[i]["read"];
-    div.appendChild(h1);
-    div.appendChild(p);
-    bookCards.appendChild(div);
-    }
-}
-displayBook(myLibrary);
 
 // add an event listener to the New Book button that pops out the modal by removing display none
 const newBookBtn = document.getElementById("new-book-btn");
@@ -78,8 +65,8 @@ newBookBtn.addEventListener("click", displayToggle);
 
 // add an event listener to the Add Book button to submit the form's inputs and call the addBookToLibrary function and displayBook function
 // also closing the modal by adding display none back to it
-const bookTitle = document.getElementById("book-title").value;
-const bookAuthor = document.getElementById("author").value;
+// const bookTitle = document.getElementById("book-title").value;
+// const bookAuthor = document.getElementById("author").value;
 
 
 // event listeners to hide the modal when clicking the cancel button, modal background, or pressing the escape key
